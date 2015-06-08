407b38a45fdbdd25a1eb7da10ae75d0a10d82228
ndnhuy <ndnhuy2504@gmail.com>
2015-06-08 17:52:16 +0700

    - Download feeds from link by using Google Feed API

--- app/src/main/java/example/com/sunreader/FeedItemsFragment.java
+++ app/src/main/java/example/com/sunreader/FeedItemsFragment.java
@@ -1,27 +1,28 @@
 package example.com.sunreader;
 
-import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import org.xmlpull.v1.XmlPullParserException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
-import example.com.sunreader.data.RssParser;
 import example.com.sunreader.value_object.RssItem;
 
 
@@ -46,17 +47,17 @@ public class FeedItemsFragment extends Fragment {
         mRootView = inflater.inflate(R.layout.rss_reader_fragment, container, false);
         new RssFeedsDownloader().execute("http://www.pcworld.com/index.rss");
 
-        ListView listView = (ListView) mRootView.findViewById(R.id.listview_feeds);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String feedTitle = (String) adapterView.getItemAtPosition(position);
-                // Call detail item activity
-                Intent intent = new Intent(getActivity(), DetailItemActivity.class);
-                intent.putExtra(DetailItemActivity.MY_INTENT_MESSAGE, feedTitle);
-                getActivity().startActivity(intent);
-            }
-        });
+//        ListView listView = (ListView) mRootView.findViewById(R.id.listview_feeds);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String feedTitle = (String) adapterView.getItemAtPosition(position);
+//                // Call detail item activity
+//                Intent intent = new Intent(getActivity(), DetailItemActivity.class);
+//                intent.putExtra(DetailItemActivity.MY_INTENT_MESSAGE, feedTitle);
+//                getActivity().startActivity(intent);
+//            }
+//        });
 
         return mRootView;
     }
@@ -65,27 +66,55 @@ public class FeedItemsFragment extends Fragment {
 
         @Override
         protected RssItem[] doInBackground(String... urlStrs) {
-            List<RssItem> rssItems = null;
-            RssParser parser = new RssParser();
             try {
-                publishProgress();
-                URL url = new URL(urlStrs[0]);
-                URLConnection urlConnection = url.openConnection();
-                InputStream inputStream = urlConnection.getInputStream();
-                rssItems = parser.parse(inputStream);
+                final String RSS_BASE_URL = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&";
+                final String QUERY_PARAM = "q";
 
+                Uri builtUri = Uri.parse(RSS_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, urlStrs[0])
+                        .build();
+                URL url = new URL(builtUri.toString());
 
-            } catch (XmlPullParserException e) {
+                // Create a request to Google Feed API, open the connection
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                Log.v(LOG_TAG, "Connect successfully.");
+
+                // Read the input stream into a String
+                InputStream inputStream = connection.getInputStream();
+                if (inputStream == null) {
+                    Log.v(LOG_TAG, "Input stream is empty ");
+                    return null;
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer buffer = new StringBuffer();
+                String line;
+
+                int i = 0;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                JSONObject json = new JSONObject(buffer.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
-            RssItem[] rssItemsArr = new RssItem[rssItems.size()];
-            rssItems.toArray(rssItemsArr);
-
-
-            return rssItemsArr;
+            return null;
         }
 
         @Override
@@ -95,20 +124,20 @@ public class FeedItemsFragment extends Fragment {
 
         @Override
         protected void onPostExecute(RssItem[] rssItems) {
-            ArrayList<String> feedTitles = new ArrayList<String>();
-            for (RssItem item : rssItems) {
-                feedTitles.add(item.getTitle());
-            }
-
-            mFeedItemsAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_rss_feeds,
-                    R.id.list_rss_feed_textview,
-                    feedTitles
-            );
-
-            ListView listView = (ListView) mRootView.findViewById(R.id.listview_feeds);
-            listView.setAdapter(mFeedItemsAdapter);
+//            ArrayList<String> feedTitles = new ArrayList<String>();
+//            for (RssItem item : rssItems) {
+//                feedTitles.add(item.getTitle());
+//            }
+//
+//            mFeedItemsAdapter = new ArrayAdapter<String>(
+//                    getActivity(),
+//                    R.layout.list_rss_feeds,
+//                    R.id.list_rss_feed_textview,
+//                    feedTitles
+//            );
+//
+//            ListView listView = (ListView) mRootView.findViewById(R.id.listview_feeds);
+//            listView.setAdapter(mFeedItemsAdapter);
         }
     }
 }
